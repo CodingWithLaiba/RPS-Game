00fd97aed1abed5748bc132d1393e6a7fa99e204
Laiba Ijaz <laibaijaz904@gmail.com>
2026-05-31 16:20:18 +0500

    Create new scroeBoard.jsx

---
+++ src/components/ScoreBoard.jsx
@@ -0,0 +1,9 @@
+
+export default function ScoreBoard() {
+  return (
+    <div className="border-3 border-gray-500 rounded-xl p-6 ">
+     
+      <h1 className="text-3xl text-center text-white font-bold tracking-widest uppercase mb-4">Rock</h1>
+    </div>
+  )
+}
