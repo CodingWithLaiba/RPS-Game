118a1eda45cbfd3015da4708b55a102a94c3bfcd
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-01 20:47:42 +0500

    Refactor GameBoard component to update choices layout and improve styling

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -4,68 +4,73 @@ import scissors from "../assets/images/icon-scissors.svg";
 import lizard from "../assets/images/icon-lizard.svg";
 import spock from "../assets/images/icon-spock.svg";
 import pentagon from "../assets/images/bg-pentagon.svg";
-import GameChoice from "./GameChoice"
+
+import GameChoice from "./GameChoice";
+
 export default function GameBoard() {
-  const choices = [
-  {
-    type: "scissors",
-    icon: scissors,
-    borderColor: "border-yellow-500",
-    position: "top-[0px] left-1/2 -translate-x-1/2",
-  },
-  {
-    type: "paper",
-    icon: paper,
-    borderColor: "border-blue-500",
-    position: "top-[100px] right-0",
-  },
-  {
-    type: "rock",
-    icon: rock,
-    borderColor: "border-red-500",
-    position: "bottom-0 right-[50px]",
-  },
-  {
-    type: "lizard",
-    icon: lizard,
-    borderColor: "border-purple-500",
-    position: "bottom-0 left-[50px]",
-  },
-  {
-    type: "spock",
-    icon: spock,
-    borderColor: "border-cyan-500",
-    position: "top-[100px] left-0",
-  },
-];
   const handleSelect = (choice) => {
-  console.log(choice);
-};
+    console.log(choice);
+  };
 
-return (
-  <div className="relative w-500 h-450">
-    
-    <img
-      src={pentagon}
-      alt="pentagon"
-      className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
-    />
+  const choices = [
+    {
+      type: "scissors",
+      icon: scissors,
+      borderColor: "border-yellow-500",
+      grid: "col-start-2 row-start-1",
+    },
+    {
+      type: "spock",
+      icon: spock,
+      borderColor: "border-cyan-500",
+      grid: "col-start-1 row-start-2",
+    },
+    {
+      type: "paper",
+      icon: paper,
+      borderColor: "border-blue-500",
+      grid: "col-start-3 row-start-2",
+    },
+    {
+      type: "lizard",
+      icon: lizard,
+      borderColor: "border-purple-500",
+      grid: "col-start-1 row-start-3",
+    },
+    {
+      type: "rock",
+      icon: rock,
+      borderColor: "border-red-500",
+      grid: "col-start-3 row-start-3",
+    },
+  ];
 
-    {choices.map((choice) => (
-      <div
-        key={choice.type}
-        className={`absolute ${choice.position}`}
-      >
-        <GameChoice
-          type={choice.type}
-          icon={choice.icon}
-          borderColor={choice.borderColor}
-          onSelect={handleSelect}
-        />
+  return (
+    <div className="relative w-full max-w-[600px] h-[420px] mx-auto">
+      <img
+        src={pentagon}
+        alt="pentagon"
+        className="absolute
+top-1/2
+left-1/2
+w-[260px]
+md:w-[320px]
+-translate-x-1/2
+-translate-y-1/2"
+      />
+
+      <div className="relative z-10 grid grid-cols-3 grid-rows-3 h-full place-items-center">
+        {choices.map((choice) => (
+          <div key={choice.type} className={choice.grid}>
+            <GameChoice
+              type={choice.type}
+              icon={choice.icon}
+              borderColor={choice.borderColor}
+              onSelect={handleSelect}
+            />
+          </div>
+        ))}
       </div>
-    ))}
-  </div>
-);
+    </div>
+  );
 }
-
-  
