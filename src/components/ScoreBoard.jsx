1e3bb2582fe98b40fb21c228caa67b181927c4fc
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-01 16:02:54 +0500

    Adjust the padding of header

--- src/components/ScoreBoard.jsx
+++ src/components/ScoreBoard.jsx
@@ -1,6 +1,6 @@
 export default function ScoreBoard({ score }) {
   return (
-    <div className="border-3 border-gray-500 rounded-xl p-4 h-39 w-180 flex flex-row items-center justify-between">
+    <div className="border-3 border-gray-500 rounded-xl px-6 py-4 md:py-5 w-180 flex flex-row items-center justify-between">
       <div className="text-left text-white font-bold text-3xl leading-none uppercase">
         <h1>Rock</h1>
         <h1>Paper</h1>
@@ -8,7 +8,7 @@ export default function ScoreBoard({ score }) {
         <h1>Lizard</h1>
         <h1>Spock</h1>
       </div>
-      <div className="bg-white h-full p-10 border-0 rounded-lg flex flex-col justify-center items-center">
+      <div className="bg-white h-full py-8 px-10 border-0 rounded-lg flex flex-col justify-center items-center">
         <p className="text-xl text-center text-blue-700 font-bold tracking-wider uppercase">
           Score
         </p>
