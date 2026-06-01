8962384469b0ed3dca485cfbcb74c8d69a93b1df
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-01 20:26:21 +0500

    Refactor GameBoard component to update choices structure and add positioning

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -5,16 +5,42 @@ import lizard from "../assets/images/icon-lizard.svg";
 import spock from "../assets/images/icon-spock.svg";
 import GameChoice from "./GameChoice"
 export default function GameBoard() {
-  const choices =[
-    { type: "rock", icon: rock, borderColor: "border-red-500" },
-    { type: "paper", icon: paper, borderColor: "border-blue-500" },
-    { type: "scissors", icon: scissors, borderColor: "border-yellow-500" },
-    { type: "lizard", icon: lizard, borderColor: "border-green-500" },
-    { type: "spock", icon: spock, borderColor: "border-purple-500" },
-  ]
+  const choices = [
+  {
+    type: "scissors",
+    icon: scissors,
+    borderColor: "border-yellow-500",
+    position: "top-[0px] left-1/2 -translate-x-1/2",
+  },
+  {
+    type: "paper",
+    icon: paper,
+    borderColor: "border-blue-500",
+    position: "top-[100px] right-0",
+  },
+  {
+    type: "rock",
+    icon: rock,
+    borderColor: "border-red-500",
+    position: "bottom-0 right-[50px]",
+  },
+  {
+    type: "lizard",
+    icon: lizard,
+    borderColor: "border-purple-500",
+    position: "bottom-0 left-[50px]",
+  },
+  {
+    type: "spock",
+    icon: spock,
+    borderColor: "border-cyan-500",
+    position: "top-[100px] left-0",
+  },
+];
   return (
     <div className="flex flex-wrap justify-center gap-8">
       {choices.map((choice) => (
+        
         <GameChoice
           key={choice.type}
           type={choice.type}
