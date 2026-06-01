ed1de88a284d6fc89e05a7b7444d53467c784bad
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-01 20:01:06 +0500

    Edit the header

--- src/components/ScoreBoard.jsx
+++ src/components/ScoreBoard.jsx
@@ -1,13 +1,9 @@
+import logoBonus from "../assets/images/logo-bonus.svg";
 export default function ScoreBoard({ score }) {
   return (
-    <div className="border-3 border-gray-500 rounded-xl px-6 py-4 md:py-5 w-180 flex flex-row items-center justify-between">
-      <div className="text-left text-white font-bold text-3xl leading-none uppercase">
-        <h1>Rock</h1>
-        <h1>Paper</h1>
-        <h1>Scissors</h1>
-        <h1>Lizard</h1>
-        <h1>Spock</h1>
-      </div>
+    <div className="border-3 border-gray-500 rounded-xl px-6  md:py-2 w-180 flex flex-row items-center justify-between">
+      <img src={logoBonus} alt="Bonus Logo" className="w-30 h-30" />
+
       <div className="bg-white h-full py-8 px-10 border-0 rounded-lg flex flex-col justify-center items-center">
         <p className="text-xl text-center text-blue-700 font-bold tracking-wider uppercase">
           Score
