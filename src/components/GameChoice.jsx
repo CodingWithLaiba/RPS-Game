deb5ada47f88f5f1ad883d9332503b76696f64e6
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-01 20:00:28 +0500

    Edit the GameChoice componet

--- src/components/GameChoice.jsx
+++ src/components/GameChoice.jsx
@@ -14,7 +14,7 @@ export default function GameChoice({ type, icon, borderColor, onSelect }) {
         hover:scale-105
       `}
     >
-      <div className="w-24 h-24 rounded-full bg-white flex items-center justify-center shadow-inner">
+      <div className="w-25 h-25 rounded-full bg-white flex items-center justify-center shadow-inner">
         <img src={icon} alt={type} className="w-12 h-12 object-contain" />
       </div>
     </button>
