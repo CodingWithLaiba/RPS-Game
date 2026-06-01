533c4b90e68dd9589ae522d33598c6ccb1d190dd
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-01 19:03:30 +0500

    add the code in Gamechoice

--- src/components/GameChoice.jsx
+++ src/components/GameChoice.jsx
@@ -1,6 +1,22 @@
-
-export default function GameChoice() {
+export default function GameChoice({ type, icon, borderColor, onSelect }) {
   return (
-    <div>GameChoice</div>
-  )
+    <button
+      onClick={() => onSelect(type)}
+      className={`
+        w-32 h-32
+        rounded-full
+        ${borderColor}
+        border-14
+        flex items-center justify-center
+        shadow-lg
+        cursor-pointer
+        transition-transform duration-200
+        hover:scale-105
+      `}
+    >
+      <div className="w-24 h-24 rounded-full bg-white flex items-center justify-center shadow-inner">
+        <img src={icon} alt={type} className="w-12 h-12 object-contain" />
+      </div>
+    </button>
+  );
 }
