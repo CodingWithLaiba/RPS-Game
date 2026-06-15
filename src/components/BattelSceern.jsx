5b377e473ca6feadf0b3133a8525b0c75725f2e4
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-15 16:00:20 +0500

    Refactor BattleScreen component for improved layout and clarity

--- src/components/BattelSceern.jsx
+++ src/components/BattelSceern.jsx
@@ -1,7 +1,17 @@
 export default function BattleScreen({ selectedChoice }) {
   return (
-    <div className="text-white text-3xl">
-      YOU PICKED {selectedChoice.type}
+    <div className="text-center text-white">
+      <p className="text-xl mb-6">YOU PICKED</p>
+
+      <img
+        src={selectedChoice.icon}
+        alt={selectedChoice.type}
+        className="w-20 mx-auto"
+      />
+
+      <p className="mt-4 text-2xl uppercase">
+        {selectedChoice.type}
+      </p>
     </div>
   );
 }
