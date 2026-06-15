0c2eedd36d55957f9e615e8e7be89b610b8dc7d7
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-15 15:52:55 +0500

    Fix import typo for BattleScreen component and adjust rendering logic in App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@
 import { useState } from "react";
 import GameBoard from "./components/GameBoard";
 import ScoreBoard from "./components/ScoreBoard";
-const [selectedchoice, setSelectedChoice] = useState(null);
+import BattleScreen from "./components/BattelSceern";
+
 function App() {
+  const [selectedchoice, setSelectedChoice] = useState(null);
   return (
     <>
       <div className="">
         <ScoreBoard score={12} />
       </div>
       <div className="flex justify-center mt-20">
-        <GameBoard onSelect={setSelectedChoice}/>
+       { !selectedchoice ? (
+         <GameBoard onSelect={setSelectedChoice}/>
+       ):(
+        <BattleScreen selectedChoice={selectedchoice} />
+       )}
       </div>
     </>
   );
