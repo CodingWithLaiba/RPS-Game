e8f59db9b6a326e8117567856a5bc7b8608c9c1d
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-15 16:22:54 +0500

    Refactor BattleScreen component to streamline layout and include result display

--- src/components/BattelSceern.jsx
+++ src/components/BattelSceern.jsx
@@ -3,15 +3,13 @@ import GameChoice from "./GameChoice";
 export default function BattleScreen({
   selectedChoice,
   computerChoice,
+  result,
 }) {
   return (
     <div className="flex items-center justify-center gap-20 text-white">
-      
       {/* Player */}
       <div className="flex flex-col items-center gap-6">
-        <p className="uppercase tracking-widest">
-          You Picked
-        </p>
+        <p className="uppercase tracking-widest">You Picked</p>
 
         <GameChoice
           type={selectedChoice.type}
@@ -20,12 +18,28 @@ export default function BattleScreen({
           onSelect={() => {}}
         />
       </div>
+      {computerChoice && (
+        <div className="flex flex-col items-center gap-4">
+          <h2 className="text-4xl font-bold">{result}</h2>
 
+          <button
+            className="
+      bg-white
+      text-black
+      px-8
+      py-3
+      rounded-lg
+      uppercase
+      tracking-widest
+      "
+          >
+            Play Again
+          </button>
+        </div>
+      )}
       {/* House */}
       <div className="flex flex-col items-center gap-6">
-        <p className="uppercase tracking-widest">
-          The House Picked
-        </p>
+        <p className="uppercase tracking-widest">The House Picked</p>
 
         {!computerChoice ? (
           <div className="w-24 h-24 rounded-full bg-slate-900/60" />
@@ -38,7 +52,6 @@ export default function BattleScreen({
           />
         )}
       </div>
-
     </div>
   );
 }
