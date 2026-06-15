97a82158d07c0e0e722472ada9b17720be3e26a0
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-15 15:32:08 +0500

    fix the positions of the buttons

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -17,63 +17,59 @@ export default function GameBoard() {
       type: "scissors",
       icon: scissors,
       borderColor: "border-yellow-500",
-      position: "top-0 left-1/2 -translate-x-1/2",
+      position: "top-15 left-1/2 -translate-x-1/2",
     },
     {
       type: "paper",
       icon: paper,
       borderColor: "border-blue-500",
-      position: "top-[110px] right-[20px]",
+      position: "top-[150px] right-[65px]",
     },
     {
       type: "rock",
       icon: rock,
       borderColor: "border-red-500",
-      position: "bottom-[20px] right-[80px]",
+      position: "bottom-[65px] right-[108px]",
     },
     {
       type: "lizard",
       icon: lizard,
       borderColor: "border-purple-500",
-      position: "bottom-[20px] left-[80px]",
+      position: "bottom-[62px] left-[108px]",
     },
     {
       type: "spock",
       icon: spock,
       borderColor: "border-cyan-500",
-      position: "top-[110px] left-[20px]",
+      position: "top-[150px] left-[56px]",
     },
   ];
 
   return (
     <div className="relative w-[500px] h-[500px] mx-auto">
-  
-  <img
-    src={pentagon}
-    alt="pentagon"
-    className="
+      <img
+        src={pentagon}
+        alt="pentagon"
+        className="
       absolute
       top-1/2
       left-1/2
-      w-[380px]
+      w-[310px]
       -translate-x-1/2
       -translate-y-1/2
     "
-  />
-
-  {choices.map((choice) => (
-    <div
-      key={choice.type}
-      className={`absolute ${choice.position}`}
-    >
-      <GameChoice
-        type={choice.type}
-        icon={choice.icon}
-        borderColor={choice.borderColor}
-        onSelect={handleSelect}
       />
+
+      {choices.map((choice) => (
+        <div key={choice.type} className={`absolute ${choice.position}`}>
+          <GameChoice
+            type={choice.type}
+            icon={choice.icon}
+            borderColor={choice.borderColor}
+            onSelect={handleSelect}
+          />
+        </div>
+      ))}
     </div>
-  ))}
-</div>
   );
 }
