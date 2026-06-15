19500daa979df7b58d05a4757b6419d122c7242f
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-15 15:44:57 +0500

    Refactor App component to manage selected choice state and pass it to GameBoard

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 // import { useState } from 'react'
+import { useState } from "react";
 import GameBoard from "./components/GameBoard";
 import ScoreBoard from "./components/ScoreBoard";
-
+const [selectedchoice, setSelectedChoice] = useState(null);
 function App() {
   return (
     <>
@@ -9,7 +10,7 @@ function App() {
         <ScoreBoard score={12} />
       </div>
       <div className="flex justify-center mt-20">
-        <GameBoard />
+        <GameBoard onSelect={setSelectedChoice}/>
       </div>
     </>
   );
